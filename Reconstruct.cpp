8f8b40a1461639f5c91080fe1136f066805a232c
Gautham J.S <42381456+Gautham-JS@users.noreply.github.com>
2020-08-14 14:55:55 +0530

    Update Reconstruct.cpp

--- Reconstruct.cpp
+++ Reconstruct.cpp
@@ -7,22 +7,7 @@
 using namespace std;
 using namespace cv;
 using namespace cv::sfm;
-static void help() {
-  cout
-      << "\n------------------------------------------------------------------------------------\n"
-      << " This program shows the multiview reconstruction capabilities in the \n"
-      << " OpenCV Structure From Motion (SFM) module.\n"
-      << " It reconstruct a scene from a set of 2D images \n"
-      << " Usage:\n"
-      << "        example_sfm_scene_reconstruction <path_to_file> <f> <cx> <cy>\n"
-      << " where: path_to_file is the file absolute path into your system which contains\n"
-      << "        the list of images to use for reconstruction. \n"
-      << "        f  is the focal length in pixels. \n"
-      << "        cx is the image principal point x coordinates in pixels. \n"
-      << "        cy is the image principal point y coordinates in pixels. \n"
-      << "------------------------------------------------------------------------------------\n\n"
-      << endl;
-}
+
 static int getdir(const string _filename, vector<String> &files)
 {
   ifstream myfile(_filename.c_str());
