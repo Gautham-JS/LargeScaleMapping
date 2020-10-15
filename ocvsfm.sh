bc6538761017abbe8fe52d1ece2cff2514daf479
Gautham J.S <42381456+Gautham-JS@users.noreply.github.com>
2020-10-15 13:38:50 +0530

    Update ocvsfm.sh

--- ocvsfm.sh
+++ ocvsfm.sh
@@ -1,11 +1,25 @@
 echo "Enter fname"
 read fname
 echo "Compiling $fname.cxx"
-g++ $fname -o app `pkg-config --cflags --libs opencv`
-echo "Enter Args[3]"
+
+g++ $fname.cpp -o app `pkg-config --cflags --libs opencv`
+if [ "$?" = "0" ]; then
+  echo "Compiled"
+else
+  echo "Terminate"
+  exit 1
+fi
+
+echo "Enter Args focal length, Cx, Cy"
 read f
 read cx
 read cy
 echo "Building ./$fname with args $f,$cx,$cy"
-./app aths.txt $f $cx $cy
 
+./app aths.txt $f $cx $cy
+if [ "$?" = "0" ]; then
+  echo " "
+else
+  echo "Terminate with exit 1"
+  exit 1
+fi
