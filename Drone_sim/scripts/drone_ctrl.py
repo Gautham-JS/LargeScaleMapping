6d8ffdafd0f0fe6440bcc3939094dc8b429fc400
gautham <gj8938@srmist.edu.in>
2020-11-08 17:55:27 +0530

    ROS Simulation added

---
+++ Drone_sim/scripts/drone_ctrl.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+import math as mt
+
+from std_msgs.msg import Float32, Float32MultiArray, Float64, Float64MultiArray
+from geometry_msgs.msg import Twist, Pose
+
+
